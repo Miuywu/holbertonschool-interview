9ebb240669883ab66b47d314fa1d3b55d6579a4b
Miuywu <859@holbertonschool.com>
2020-04-20 14:39:17 -0700

    cleanup 3

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -6,24 +6,18 @@ def canUnlockAll(boxes):
     keys = [0]
     for box in boxes:
         if i == len(boxes) - 1:
-            """print("done!")"""
             break
-        """print("need ", i + 1, ", ", end='')"""
         for key in keys:
             tmp = []
-            if key < len(boxes) - 1:
+            if key < len(boxes):
                 for new_keys in boxes[key]:
                     tmp.append(new_keys)
             for new_keys in tmp:
                 if new_keys not in keys:
                     keys.append(new_keys)
         if i + 1 not in keys:
-            """print(keys)"""
             return False
-        else:
-            """print(i + 1, " found!")"""
         i += 1
-    """print(keys)"""
     return True
 
 
