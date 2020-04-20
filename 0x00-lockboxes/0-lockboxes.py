6ea5a97b18c3ba342c5490e12ecb4ac8fde278f2
Miuywu <859@holbertonschool.com>
2020-04-20 01:51:38 -0700

    exit condition changed

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -5,25 +5,31 @@ def canUnlockAll(boxes):
     i = 0
     keys = []
     for box in boxes:
-        if box == boxes[len(boxes) - 1]:
+        if i == len(boxes) - 1:
+            """print("done!")"""
             break
+        """print("need ", i + 1, ", ", end='')"""
         if i == 0:
             for new_keys in box:
                 if new_keys not in keys:
                     keys.append(new_keys)
-            i += 1
-            continue
-        for key in keys:
-            tmp = []
-            if key < len(boxes) - 1:
-                for new_keys in boxes[key]:
-                    tmp.append(new_keys)
-            for new_keys in tmp:
-                if new_keys not in keys:
-                    keys.append(new_keys)
+        else:
+            if box != []:
+                for key in keys:
+                    tmp = []
+                    if key < len(boxes) - 1:
+                        for new_keys in boxes[key]:
+                            tmp.append(new_keys)
+                    for new_keys in tmp:
+                        if new_keys not in keys:
+                            keys.append(new_keys)
         if i + 1 not in keys:
+            """print(keys)"""
             return False
+        else:
+            """print(i + 1, " found!")"""
         i += 1
+    """print(keys)"""
     return True
 
 
