8aa31b24493a9eb45a9b8580db291b03b4440056
Miuywu <859@holbertonschool.com>
2020-05-05 23:30:10 -0700

    firsrt commit

---
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - creates node in tree
+ * @parent: pointer to parent node
+ * @value: the value to store in the node
+ * Return: new node or null if fails
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new = calloc(1, sizeof(*node));
+
+	if (new != NULL)
+	{
+		new->n = value;
+		new->parent = parent;
+	}
+	return (new);
+}
