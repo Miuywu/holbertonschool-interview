de5f39cb59fe9eac5e646d6404bb15223f285cdd
Miuywu <859@holbertonschool.com>
2020-05-20 10:54:06 +0000

    betty

--- 0x04-sandpiles/0-sandpiles.c
+++ 0x04-sandpiles/0-sandpiles.c
@@ -20,6 +20,32 @@ void print_pile(int pile[3][3])
 		}
 }
 /**
+ * pile_add - adds two piles
+ * @pile1: sand pile
+ * @pile2: sand pile
+ * Return: 1 if unstable, 0 otherwise
+ */
+int pile_add(int pile1[3][3], int pile2[3][3])
+{
+	int a, b, unstable = 0;
+
+	for (a = 0; a < 3; a++)
+		for (b = 0; b < 3; b++)
+			pile1[a][b] += pile2[a][b];
+	for (a = 0; a < 3; a++)
+		for (b = 0; b < 3; b++)
+			if (pile1[a][b] > 3)
+			{
+				unstable++;
+			}
+	if (unstable)
+	{
+		printf("=\n");
+		return (1);
+	}
+	return (0);
+}
+/**
  * sandpiles_sum - adds sandpiles then shows progression toward stable sandpile
  * @grid1: first sandpile
  * @grid2: second sandpile
@@ -27,24 +53,13 @@ void print_pile(int pile[3][3])
  */
 void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
-	int a, b, unstable = 0, grid3[3][3];
+	int a, b, unstable = 1, grid3[3][3];
 
-	for (a = 0; a < 3; a++)
-		for (b = 0; b < 3; b++)
-			grid1[a][b] += grid2[a][b];
-	for (a = 0; a < 3; a++)
-		for (b = 0; b < 3; b++)
-			if (grid1[a][b] > 3)
-			{
-				unstable++;
-			}
-	if (unstable)
-		printf("=\n");
-	unstable = 1;
+	if (pile_add(grid1, grid2) == 0)
+		return;
 	while (unstable > 0)
 	{
-		unstable = 0;
-		for (a = 0; a < 3; a++)
+		for (a = 0, unstable = 0; a < 3; a++)
 			for (b = 0; b < 3; b++)
 				if (grid1[a][b] > 3)
 				{
@@ -58,9 +73,8 @@ void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 				printf("=\n");
 			for (a = 0; a < 3; a++)
 				for (b = 0; b < 3; b++)
-				{
-				if (grid3[a][b] == 1)
-				{
+					if (grid3[a][b] == 1)
+					{
 					grid1[a][b] -= 4;
 					if (a > 0)
 						grid1[a - 1][b] += 1;
@@ -70,8 +84,7 @@ void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 						grid1[a + 1][b] += 1;
 					if (b < 2)
 						grid1[a][b + 1] += 1;
-				}
-				}
+					}
 			for (a = 0; a < 3; a++)
 				for (b = 0; b < 3; b++)
 					grid3[a][b] = 0;
