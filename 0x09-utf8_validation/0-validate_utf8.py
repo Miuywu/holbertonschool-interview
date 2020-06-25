78a44daf4fe6ed33be15625e4c9f4ed89e1d0d91
Minh Huy <859@hobertonschool.com>
2020-06-25 02:53:29 -0400

    doc

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -3,7 +3,7 @@
 
 
 def validUTF8(data):
-    '''Checks UTF-8 format'''
+    """Checks UTF-8 format"""
     bit = 0
 
     for number in data:
@@ -11,13 +11,13 @@ def validUTF8(data):
             bit -= 1
             if (((n & 0xff) >> 6) != 2):
                 return False
-        elif (n & 0xF0) == 0xF0:  """4"""
+        elif (n & 0xF0) == 0xF0:
             bit = 3
-        elif (n & 0xE0) == 0xE0:  """3"""
+        elif (n & 0xE0) == 0xE0:
             bit = 2
-        elif (n & 0xC0) == 0xC0:  """2"""
+        elif (n & 0xC0) == 0xC0:
             bit = 1
-        elif not (n & 0x80):      """1"""
+        elif not (n & 0x80):
             bit = 0
         else:
             return False
