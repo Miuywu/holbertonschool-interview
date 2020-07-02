ba4f051c8051051079b2b621500a16efebdacd4b
Miuywu <859@holbertonschool.com>
2020-07-02 06:23:07 +0000

    betty

--- 0x0A-slide_line/0-slide_line.c
+++ 0x0A-slide_line/0-slide_line.c
@@ -8,21 +8,21 @@
  */
 int slide_line(int *line, size_t size, int direction)
 {
-	int s = (int)size, d = 1, a, b, c, z = 0;
+	int s = (int)size, d = 1, a, b, c, z = 0, y = 0, w = 1;
 
-	if (!line ||(direction != SLIDE_LEFT && direction != SLIDE_RIGHT))
+	if (!line || (direction != SLIDE_LEFT && direction != SLIDE_RIGHT))
 		return (0);
 	if (direction == SLIDE_RIGHT)
-	{
-		d = -1;
+	{	d = -1;
 		z = s;
+		y = s - 1;
+		w = -1;
 	}
-	for (a = z; a < s - 1; a += d)
+	for (a = z; a != y; a += d)
 	{
 		if (!line[a])
-		{
-			c = a;
-			for (b = a + d; b <= s - 1; b += d)
+		{	c = a;
+			for (b = a + d; b != y + w; b += d)
 				if (line[b])
 				{
 					line[c] = line[b];
@@ -33,7 +33,7 @@ int slide_line(int *line, size_t size, int direction)
 		if (!line[a + d])
 		{
 			c = a + d;
-			for (b = a + (2 * d); b <= s - 1; b += d)
+			for (b = a + (2 * d); b != y + w; b += d)
 				if (line[b])
 				{
 					line[c] = line[b];
