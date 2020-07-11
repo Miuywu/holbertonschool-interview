5abea389a9f26651ff97dcbc8790599c5ceb6b11
Minh Huy <859@hobertonschool.com>
2020-07-11 00:56:50 -0400

    checks

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -4,21 +4,8 @@
 
 def validUTF8(data):
     """Checks UTF-8 format"""
-    bit = 0
-    for byte in data:
-        if bit:
-            if byte >> 6 != 0b10:
-                return False
-            bit -= 1
-        else:
-            if byte >> 7 == 0:
-                continue
-            elif byte >> 3 == 0b11110:
-                bit = 3
-            elif byte >> 4 == 0b1110:
-                bit = 2
-            elif byte >> 5 == 0b110:
-                bit = 1
-            else:
-                return False
-    return bit == 0
+    try:
+        bytes(x & 0xff for x in data).decode()
+        return True
+    except:
+        return False
