1cf3809dbb5a3148ab83899a77550bcdab0ebf67
Miuywu <859@holbertonschool.com>
2020-08-20 01:12:43 -0400

    stars

---
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+const request = require('request');
+
+request.get(`http://swapi.co/api/films/${process.argv[2]}`, async (error, response, body) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+  const CharactersList = JSON.parse(body).characters;
+  for (let a = 0; a < CharactersList.length; a++) {
+    const name = await nameRequest(CharactersList[a]);
+    console.log(name);
+  }
+});
+
+function nameRequest (char_url) {
+  const name = new Promise(
+    (resolve, reject) => {
+      request(char_url, (error, response, body) => {
+        if (error) {
+          console.error('error:', error)
+        }
+        resolve(JSON.parse(body).name);
+      });
+    });
+  return name;
+}
