250ce2709754240f92118b38a046074e7ebdd631
Miuywu <859@holbertonschool.com>
2020-10-16 18:45:40 -0400

    circularll

---
+++ 0x17-find_the_loop/0-find_the_loop.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+/**
+ * find_listint_loop - checks for a circular linked list
+ * @head: linked list
+ * Return: 1 if circular, else 0
+ */
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *tort = head, *hare = head;
+
+	while (hare && hare->next)
+	{
+		tort = tort->next;
+		hare = hare->next->next;
+		if (tort == hare)
+		{
+			tort = head;
+			while (tort != hare)
+			{
+				tort = tort->next;
+				hare = hare->next;
+			}
+			return (tort);
+		}
+	}
+	return (NULL);
+}
